ecdb0e1edfa5c18315e1d8f7580ca156b4a47179
jntun <justin@jntun.com>
2024-03-22 17:15:27 -0700

    Render: Introduce render subsystem

---
+++ render/render.c
@@ -0,0 +1,12 @@
+//
+// Created by Justin Tunheim on 2/26/24.
+//
+
+#include "render.h"
+
+cell99_game_t render(struct renderer *r, cell99_game_t game)
+{
+    /* TODO: Implement OpenGL */
+    return game;
+}
+
