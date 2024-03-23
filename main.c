73c1fce7b0057dd8c5e63de5c28633423b281c90
jntun <justin@jntun.com>
2024-03-22 17:33:51 -0700

    cell99: Introduce engine struct

--- main.c
+++ main.c
@@ -1,21 +1,49 @@
 #include <stdlib.h>
 #include <flecs.h>
 
+#include "render.h"
 #include "util.h"
 #include "game.h"
 
-int main() {
-    printf("Hello, " KTXT(KRED, "cell99") "!\n");
+struct engine {
+    uint8_t running;
 
-    struct cell99 game = init();
-    for (game = start(game); game.running; game = run(game)) {};
+    cell99_game_t game;
+    struct renderer renderer;
+};
+
+void input(struct engine *engine)
+{
+    SDL_Event e;
+
+    while (SDL_PollEvent(&e)) {
+        if (e.type == SDL_QUIT) {
+            engine->running = false;
+        }
+    }
+}
+
+int main()
+{
+    printf("Hello, " KTXT(KRED, "cell99") "!\n");
+    struct engine cell99 = {1, init(), renderer_default()};
+
+    cell99.game = start(cell99.game);
+    do {
+        if (cell99.game.running) cell99.game = run(cell99.game);
+
+        // The cel99::renderer calling render() requires ownership of the game it will be rendering.
+        cell99.game = render(&cell99.renderer, cell99.game);
+        input(&cell99);
+    } while (cell99.running);
 
     ecs_time_t stop;
     ecs_os_get_time(&stop);
-    ecs_time_t runtime = ecs_time_sub(stop, game.start);
-    printf("ran: %lld ticks\nstart: %d stop: %d total: %d\n", game.epoch, game.start.nanosec, stop.nanosec, runtime.nanosec);
+    ecs_time_t runtime = ecs_time_sub(stop, cell99.game.start);
+    printf("ran: %lld ticks\nstart: %d stop: %d total: %d\n", cell99.game.epoch, cell99.game.start.nanosec, stop.nanosec, runtime.nanosec);
 
 EXIT:
-    close(game);
+    close(cell99.game);
+
     return 0;
 }
