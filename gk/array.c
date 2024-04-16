29bdebf200104beb54b37da0e0b128620c8bd037
jntun <justin@jntun.com>
2024-04-15 22:06:29 -0700

    gk: Add generic array type

---
+++ gk/array.c
@@ -0,0 +1,57 @@
+//
+// Created by Justin Tunheim on 4/15/24.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+#include "array.h"
+
+void array_push(array_t *a, void *item)
+{
+    if (sizeof(*item) != a->size) {
+        fprintf(stderr, "%s@%d: Type confusion:\n\tsizeof(item)=%zu does not match array->size=%zu.\n",
+                __FUNCTION__,
+                __LINE__,
+                sizeof(*item),
+                a->size
+        );
+        return;
+    }
+    if (a->count == a->capacity) {
+array_grow(a);
+    }
+    void *insert = a->buff + (a->count * a->size);
+    memcpy(insert, item, a->size);
+    a->count++;
+}
+
+array_t array_init(size_t size, size_t capacity)
+{
+    array_t a = {
+            .size = size,
+            .count = 0,
+            .capacity = capacity ? capacity : CELL99_ARRAY_DEFAULT_COUNT,
+            .buff = NULL
+    };
+    a.buff = malloc(a.size * a.capacity);
+    return a;
+}
+
+void array_grow(array_t *a)
+{
+    size_t new_length = a->capacity * 2;
+    char *new_buf = malloc(a->size * new_length);
+    memcpy(new_buf, a->buff, (a->size * a->capacity));
+    a->capacity = new_length;
+    free(a->buff);
+    a->buff = new_buf;
+}
+
+void array_free(array_t *a)
+{
+    a->size = 0;
+    a->count = 0;
+    a->capacity = 0;
+    free(a->buff);
+}
---
+++ include/array.h
@@ -0,0 +1,26 @@
+//
+// Created by Justin Tunheim on 4/15/24.
+//
+
+#ifndef CELL99_ARRAY_H
+#define CELL99_ARRAY_H
+
+#include <stdlib.h>
+
+#define CELL99_ARRAY_DEFAULT_COUNT 16
+
+typedef struct _c99_array {
+    size_t size;
+    size_t count;
+    size_t capacity;
+    void  *buff;
+} array_t;
+
+void             array_push(array_t *a, void *item);
+array_t          array_init(size_t size, size_t capacity);
+void             array_grow(array_t *a);
+void             array_free(array_t *a);
+
+#define array_new(type) array_init(sizeof(type), 0)
+
+#endif //CELL99_ARRAY_H
